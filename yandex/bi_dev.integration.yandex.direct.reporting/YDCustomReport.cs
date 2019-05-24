f813bbce9b52fcdf93b27621a207acebdd072271
Алексей Шамшур <a.shamshur@tnomer.ru>
2019-05-24 13:24:54 +0300

    ovrd YD ToDataTable

--- yandex/bi_dev.integration.yandex.direct.reporting/YDCustomReport.cs
+++ yandex/bi_dev.integration.yandex.direct.reporting/YDCustomReport.cs
@@ -1,6 +1,7 @@
 ﻿using bi_dev.integration.reporting;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace bi_dev.integration.yandex.direct.reporting
@@ -10,6 +11,19 @@ namespace bi_dev.integration.yandex.direct.reporting
         public YDCustomReport(YDCustomReportInitializer initializer) : base(initializer)
         {
             this.Rows = new List<CustomReportRow>();
+        }
+        public override DataTable ToDataTable()
+        {
+            var dt = base.ToDataTable();
+            dt.Columns.Add("prm_date_from", typeof(DateTime));
+            dt.Columns.Add("prm_date_to", typeof(DateTime));
+            foreach(DataRow row in dt.Rows)
+            {
+                row["prm_date_from"] = initializer.DateFrom;
+                row["prm_date_to"] = initializer.DateTo;
+            }
+            return dt;
+
         }
     }
 }
