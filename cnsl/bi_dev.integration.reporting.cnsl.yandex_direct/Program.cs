85dce0c3c9a7d40518c333019516b4fef7136aca
Алексей Шамшур <a.shamshur@tnomer.ru>
2019-05-24 13:30:26 +0300

    fx main args order

--- cnsl/bi_dev.integration.reporting.cnsl.yandex_direct/Program.cs
+++ cnsl/bi_dev.integration.reporting.cnsl.yandex_direct/Program.cs
@@ -10,14 +10,14 @@ namespace bi_dev.integration.reporting.cnsl.yandex_direct
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             
             string credentialsJsonPath = args[0];
             string reportType = args[1];
-            string dateFormat = args[2];
-            DateTime eventDate = DateTime.ParseExact(args[3], dateFormat, CultureInfo.CurrentCulture);
-            string [] columns = JsonConvert.DeserializeObject<string[]>(args[4]);
+            string[] columns = JsonConvert.DeserializeObject<string[]>(args[2]);
+            string dateFormat = args[3];
+            DateTime eventDate = DateTime.ParseExact(args[4], dateFormat, CultureInfo.CurrentCulture);
             string dbConnectionStringPath = File.ReadAllText(args[5]);
             string destTblName = args[6];
             
@@ -44,7 +44,7 @@ namespace bi_dev.integration.reporting.cnsl.yandex_direct
                 )
             );
             gsrmsm.Save(rep);
-            
+            return 0;
         }
     }
 }
